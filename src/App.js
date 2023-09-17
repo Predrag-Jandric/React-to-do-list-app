650c3b5075850062cb6a2aa416dab918c193f64f
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-17 23:39:26 +0200

    project initiated

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Item from "./components/Item";
+
+function App() {
+
+  
+
+  return (
+    <main className="main">
+      
+    </main>
+  );
+}
+
+export default App;
