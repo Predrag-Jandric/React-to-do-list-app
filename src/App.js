9b0ccc9e4a99d2add27cd2ede34bc25bdf64a4d3
predrag jandric <predrag.jandric.bg@gmail.com>
2023-09-17 23:40:12 +0200

    some logic added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,35 @@ import Item from "./components/Item";
 
 function App() {
 
-  
+  const [todos, setTodos] = useState([])
+  const [inputVal, setInputVal] = useState("")
+
+  const handleInput = (e) => {
+    setInputVal(e.target.value)
+  }
+
+  const addToDo = () => {
+    if (inputVal !== "") {
+      setTodos(prevTodo => [...prevTodo, inputVal])
+      setInputVal("")
+    }
+  }
+
+  const handleDelete = (uniqueIndex) => {
+    setTodos((prev) => prev.filter((prevTodo, prevIndex) => prevIndex !== uniqueIndex));
+  }
 
   return (
     <main className="main">
-      
+      <h1>React To-do List</h1>
+
+      <section className="addItem">
+        <input className="addItem__input" type="text" value={inputVal} placeholder="Enter task" onChange={handleInput} />
+        <button className="addItem__btn globalBtn" onClick={addToDo}>Add</button>
+      </section>
+
+      <Item todos={todos} handleDelete={handleDelete} />
+
     </main>
   );
 }
